721567987ac5025689f2b33510a586681de91acb
lautarolopez1498 <82686600+lautarolopez1498@users.noreply.github.com>
2023-03-17 08:16:53 -0300

    ordenando codigo

--- src/app.js
+++ src/app.js
@@ -1,38 +1,46 @@
-const express = require('express');
-const viewRouter = require('./routes/view.router')
-const productsRouter = require('./routes/products.router')
-const handlebars = require('express-handlebars');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+const express = require("express");
+const viewRouter = require("./routes/view.router");
+const productsRouter = require("./routes/products.router");
+const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
+const { PORT, URL_SERVICE } = require("./utils/constanst");
+mongoose.set("strictQuery", false);
 
 
-
-mongoose.connect(  'mongodb+srv://lautaro:528276@cluster0.8ag41uo.mongodb.net/?retryWrites=true&w=majority',  (error) => {
-    if (error) {
-      console.log("Cannot connect to database: ", + error);
-      process.exit();
-    } else {
-      console.log('\n***** Successful connection! *****\n');
-    }
-  }
-);
-
-
+// INNIT
 const app = express();
-const PORT = 8080;
 
-app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
 
+
+
+// middlewares
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-app.use('/', viewRouter);
-app.use('/api/products', productsRouter);
 
 
+// routes
+app.use("/", viewRouter);
+app.use("/api/products", productsRouter);
+
+
+
+// server y db
 app.listen(PORT, () => {
-  console.log(`\nServer running on port ${PORT}`);
-});
+  console.log(`\nServer running on port ${URL_SERVICE}`);
 
+  mongoose.connect(
+    "mongodb+srv://lautaro:528276@cluster0.8ag41uo.mongodb.net/?retryWrites=true&w=majority",
+    (error) => {
+      if (error) {
+        console.log("Cannot connect to database: ", +error);
+        process.exit();
+      } else {
+        console.log("\n***** Successful connection! *****\n");
+      }
+    }
+  );
+});
--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,5 +1,6 @@
 const constants = {
     URL_SERVICE: 'http://localhost:8080',
+    PORT: 8080
 };
 
 module.exports = constants;
